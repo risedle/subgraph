23e9c33cf77f0f63918a8919a8c1f965b34e4de3
Dimas Riatmodjo <dimasriat@gmail.com>
2022-06-20 03:24:34 +0700

    ✦ Add Token Info Fetch Helper

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -1,4 +1,7 @@
-import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
+import { Address, BigDecimal, BigInt } from "@graphprotocol/graph-ts";
+import { ERC20 } from "../types/RiseTokenVault/ERC20";
+import { ERC20NameBytes } from "../types/RiseTokenVault/ERC20NameBytes";
+import { ERC20SymbolBytes } from "../types/RiseTokenVault/ERC20SymbolBytes";
 
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 
@@ -20,4 +23,67 @@ export function bigDecimalExp18(): BigDecimal {
 
 export function convertEthToDecimal(eth: BigInt): BigDecimal {
 	return eth.toBigDecimal().div(exponentToBigDecimal(BI_18));
+}
+
+export function isNullEthValue(value: string): boolean {
+	return (
+		value ==
+		"0x0000000000000000000000000000000000000000000000000000000000000001"
+	);
+}
+
+export function fetchTokenSymbol(tokenAddress: Address): string {
+	const contract = ERC20.bind(tokenAddress);
+	const contractSymbolBytes = ERC20SymbolBytes.bind(tokenAddress);
+
+	// try types string and bytes32 for symbol
+	let symbolValue = "unknown";
+	const symbolResult = contract.try_symbol();
+	if (symbolResult.reverted) {
+		const symbolResultBytes = contractSymbolBytes.try_symbol();
+		if (!symbolResultBytes.reverted) {
+			// for broken pairs that have no symbol function exposed
+			if (!isNullEthValue(symbolResultBytes.value.toHexString())) {
+				symbolValue = symbolResultBytes.value.toString();
+			}
+		}
+	} else {
+		symbolValue = symbolResult.value;
+	}
+
+	return symbolValue;
+}
+
+export function fetchTokenName(tokenAddress: Address): string {
+	const contract = ERC20.bind(tokenAddress);
+	const contractNameBytes = ERC20NameBytes.bind(tokenAddress);
+
+	// try types string and bytes32 for name
+	let nameValue = "unknown";
+	const nameResult = contract.try_name();
+	if (nameResult.reverted) {
+		const nameResultBytes = contractNameBytes.try_name();
+		if (!nameResultBytes.reverted) {
+			// for broken exchanges that have no name function exposed
+			if (!isNullEthValue(nameResultBytes.value.toHexString())) {
+				nameValue = nameResultBytes.value.toString();
+			}
+		}
+	} else {
+		nameValue = nameResult.value;
+	}
+
+	return nameValue;
+}
+
+export function fetchTokenTotalSupply(tokenAddress: Address): BigInt {
+	const contract = ERC20.bind(tokenAddress);
+	const totalSupplyResult = contract.try_totalSupply();
+	return totalSupplyResult.value;
+}
+
+export function fetchTokenDecimals(tokenAddress: Address): BigInt {
+	const contract = ERC20.bind(tokenAddress);
+	const decimalResult = contract.try_decimals();
+	return BigInt.fromI32(decimalResult.value);
 }
